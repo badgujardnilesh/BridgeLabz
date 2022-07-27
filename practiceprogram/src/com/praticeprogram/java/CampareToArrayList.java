364a49bc07d0de41962630d4e9a496dd7d23277a
badgujardnilesh <104567814+badgujardnilesh@users.noreply.github.com>
2022-07-27 13:50:13 +0530

    Add files via upload

---
+++ practiceprogram/src/com/praticeprogram/java/CampareToArrayList.java
@@ -0,0 +1,25 @@
+package com.praticeprogram.java;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CampareToArrayList {
+
+
+    public class SumNaturalUsingStream {
+        public static void main(String[] args) {
+            ArrayList<Integer> integers = new ArrayList<>();
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter The Nth Natural Number: ");
+            int n = sc.nextInt();
+
+            for (int i = 0; i <= n; i++) {
+                integers.add(i);
+            }
+            Integer ans = integers.stream().reduce(0, (a, b) -> a + b);
+            System.out.println(ans);
+        }
+
+
+    }
+}
